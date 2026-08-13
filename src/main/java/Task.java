150fdcd098fe94eca5e0d33bbcd3c4d7683529eb
Linus <e1123122@u.nus.edu>
2026-08-13 23:38:34 +0800

    Created Task class

---
+++ src/main/java/Task.java
@@ -0,0 +1,29 @@
+public class Task {
+    private String task;
+    private boolean done;
+
+    private Task(String task , boolean done) {
+        this.task = task;
+        this.done = done;
+    }
+
+    public Task(String task) {
+        this(task, false);
+    }
+
+    public void mark(int index) {
+        this.done = true;
+    }
+
+    public void unmark(int index) {
+        this.done = false;
+    }
+
+    @Override
+    public String toString() {
+        if (this.done) {
+            return "[X] " + this.task;
+        }
+        return "[ ] " + this.task;
+    }
+}
