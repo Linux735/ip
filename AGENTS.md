5709be7a7ed11ea39184bc379599a06670cd0da7
Linus <e1123122@u.nus.edu>
2026-08-13 20:27:39 +0800

    Added greeting and exit message

--- AGENTS.md
+++ AGENTS.md
@@ -9,8 +9,8 @@ Unless the user says otherwise, assume that you are assisting a student working 
 # Student profile
 
 * Prior knowledge: Basic Java and OOP concepts.
-* Level of programming experience: [to be filled]
-* IDE and level of expertise: [to be filled]
+* Level of programming experience: Have worked on different small projects in multiple modules
+* IDE and level of expertise: Only use IDE for its text editing and running
 
 # Guidance for interacting with users
 
--- src/main/java/Alzara.java
+++ src/main/java/Alzara.java
@@ -1,10 +1,23 @@
-public class Duke {
+public class Alzara {
+    /**
+     * Starts the Alzara chatbot application.
+     *
+     * @param args command-line arguments supplied when the application starts
+     */
     public static void main(String[] args) {
-        String banner = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println(banner);
+        String separator = "____________________________________________________________";
+        String banner = "    _    _     ______    _    ____       _    \n"
+                + "   / \\  | |   |__  /   / \\  |  _ \\     / \\   \n"
+                + "  / _ \\ | |     / /   / _ \\ | |_) |   / _ \\  \n"
+                + " / ___ \\| |___ / /_  / ___ \\|  _ <   / ___ \\ \n"
+                + "/_/   \\_\\_____/____|/_/   \\_\\_| \\_\\ /_/   \\_\\\n";
+        System.out.println(separator);
+        System.out.print(banner);
+        System.out.println("And as it was foretold,\nI am awakened by the sound of tears.");
+        System.out.println("You find yourself face to face with the great Alzara.");
+        System.out.println("I suspect you wish me to guide you?");
+        System.out.println(separator);
+        System.out.println("Our audience has ended. Until we meet again.");
+        System.out.println(separator);
     }
 }
